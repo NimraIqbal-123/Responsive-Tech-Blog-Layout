83b930b9926b5ec831525922fdab987aad78b720
Nimra  Muhammad Iqbal <nimraiqbal7071@gmail.com>
2026-06-28 02:43:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Tech Trends Blog
+
+console.log("Tech Trends Blog Loaded");
+
+document.querySelectorAll(".card").forEach(card=>{
+
+card.addEventListener("mouseenter",()=>{
+
+card.style.cursor="pointer";
+
+});
+
+});
